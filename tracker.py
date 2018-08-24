f0eb6cf55fc7b67dc05ac6fd0c7afd8c4e4f0425
Ankith RD <31142949+AnkithDivakarRaja@users.noreply.github.com>
2018-08-24 06:14:49 -0400

    Update tracker.py
    Added new mail ID for notification

--- tracker.py
+++ tracker.py
@@ -6,7 +6,7 @@ from email.mime.multipart import MIMEMultipart
 
 def send_email(message):
 	mail_from = "ncsucarrental@gmail.com"
-	mail_to = ["araja2@ncsu.edu", "nsompal@ncsu.edu", "gyelchu@ncsu.edu"]
+	mail_to = ["araja2@ncsu.edu", "nsompal@ncsu.edu", "gyelchu@ncsu.edu", "nbangal2@ncsu.edu"]
 	
 	username = "ncsucarrental@gmail.com"
 	password = "i@mbatman"
